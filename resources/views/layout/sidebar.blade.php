14df16e8f704bc593a6eec6b4a14090ae8004e59
Quinzana <pquinzana@gmail.com>
2023-06-11 22:50:14 +0700

    template dashboard

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,130 @@
+<div class="leftside-menu">
+    
+    <!-- LOGO -->
+    <a href="#" class="logo text-center logo-light">
+        <span class="logo-lg fs-4 fw-bolder">
+            <img src="assets/images/logo_sm.png" alt="" height="16">
+            ACE RENTAL CAR
+        </span>
+        <span class="logo-sm">
+            <img src="assets/images/logo_sm.png" alt="" height="16">
+        </span>
+    </a>
+
+    <div class="h-100" id="leftside-menu-container" data-simplebar="">
+
+        <!--- Sidemenu -->
+        <ul class="side-nav">
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="/dashboard" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link">
+                    <i class="uil-home-alt"></i>
+                    <span class="badge bg-success float-end">4</span>
+                    <span> Dashboards </span>
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a href="profile" class="side-nav-link">
+                    <i class="uil-user"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarMaster" aria-expanded="false" aria-controls="sidebarEcommerce" class="side-nav-link">
+                    <i class="uil-layer-group"></i>
+                    <span> Master </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarMaster">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="konfirm">Master Customer</a>
+                        </li>
+                        <li>
+                            <a href="ambil">Master Pegawai</a>
+                        </li>
+                        <li>
+                            <a href="kembali">Master Mobil</a>
+                        </li>
+                        <li>
+                            <a href="selesai">Master </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarTransaksi" aria-expanded="false" aria-controls="sidebarEcommerce" class="side-nav-link">
+                    <i class="uil-car"></i>
+                    <span> Transaksi </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarTransaksi">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="konfirm">Konfirmasi Pembayaran</a>
+                        </li>
+                        <li>
+                            <a href="ambil">Pengambilan Mobil</a>
+                        </li>
+                        <li>
+                            <a href="kembali">Pengembalian Mobil</a>
+                        </li>
+                        <li>
+                            <a href="selesai">Selesai</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarReport" aria-expanded="false" aria-controls="sidebarCharts" class="side-nav-link">
+                    <i class="uil-chart"></i>
+                    <span>Laporan</span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarReport">
+                    <ul class="side-nav-second-level">
+                        <li>
+                            <a href="charts-brite.html">Jadwal Persewaan</a>
+                        </li>
+                        
+                        <li class="side-nav-item">
+                            <a data-bs-toggle="collapse" href="#sidebarApexCharts" aria-expanded="false" aria-controls="sidebarApexCharts">
+                                <span>Rekap Penyewaan</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarApexCharts">
+                                <ul class="side-nav-third-level">
+                                    <li>
+                                        <a href="charts-apex-area.html">Customer</a>
+                                    </li>
+                                    <li>
+                                        <a href="charts-apex-bar.html">Mobil</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="charts-sparkline.html">Penilaian</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="side-nav-item">
+                <a href="logout" class="side-nav-link">
+                    <i class="uil-exit"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
+        </ul>
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
